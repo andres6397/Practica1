f3ef2dc2230525b98b52b59fe54d579d6f0770fb
andresrpo6397 <andresrpo6397@gmail.com>
2021-11-04 16:46:06 +0000

    Se ha creado la funcion exit y en la declaracion de variables se ha añadido los contadores

--- parte1.c
+++ parte1.c
@@ -11,12 +11,43 @@ MODULE_VERSION("0.1");
 
 //Variables botones y LEDS
 static unsigned int ledRojo = 16;
-static unsigned int ledAzul = 20;
+static unsigned int ledVerde = 20;
 static unsigned int boton1 = 21;
 static unsigned int boton2 = 26;
 static unsigned int boton3 = 19;
 static unsigned int boton4 = 13;
+static unsigend int contador1 = 0; 
+static unsigend int contador2 = 0; 
+static unsigend int contador3 = 0; 
+static unsigend int contador4 = 0; 
 static bool     ledOn = 0; //Esta apagado por defecto
 
+static void __exit ebbgpio_exit(void){
+   printk(KERN_INFO "parte1.c: The button state is currently: %d\n", gpio_get_value(boton1));
+   printk(KERN_INFO "parte1.c: The button was pressed %d times\n", contador1);
+   printk(KERN_INFO "parte1.c: The button state is currently: %d\n", gpio_get_value(boton2));
+   printk(KERN_INFO "parte1.c: The button was pressed %d times\n", contador2);
+   printk(KERN_INFO "parte1.c: The button state is currently: %d\n", gpio_get_value(boton3));
+   printk(KERN_INFO "parte1.c: The button was pressed %d times\n", contador3);
+   printk(KERN_INFO "parte1.c: The button state is currently: %d\n", gpio_get_value(boton4));
+   printk(KERN_INFO "parte1.c: The button was pressed %d times\n", contador4);
 
+   gpio_set_value(ledRojo, 0);              // Turn the LED off, makes it clear the device was unloaded
+   gpio_unexport(ledRojo);                  // Unexport the LED GPIO
+   gpio_set_value(ledVerde, 0);              // Turn the LED off, makes it clear the device was unloaded
+   gpio_unexport(ledVerde);                  // Unexport the LED GPIO
+
+   free_irq(irqNumber, NULL);               // Free the IRQ number, no *dev_id required in this case
+
+   gpio_unexport(boton1);               // Unexport the Button GPIO
+   gpio_unexport(boton2);               // Unexport the Button GPIO
+
+   gpio_free(ledRojo);                      // Free the LED GPIO
+   gpio_free(ledVerde);                      // Free the LED GPIO
+   gpio_free(boton1);                      // Free the button GPIO
+   gpio_free(boton2);                      // Free the button GPIO
+   gpio_free(boton3);                      // Free the button GPIO
+   gpio_free(boton4);                   // Free the button GPIO
+   printk(KERN_INFO "GPIO_TEST: Goodbye from the LKM!\n");
+}
 
