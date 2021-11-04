520aab4fa2f9e8e65fed3c110f25a9dfed29c3b9
andresrpo6397 <andresrpo6397@gmail.com>
2021-11-04 17:51:30 +0000

    añadimos una condicion if para comprobar si el pin del led verde es valido y la variable ledRojo pasa a ser ledAzul

--- parte1.c
+++ parte1.c
@@ -10,8 +10,8 @@ MODULE_DESCRIPTION("Controlar 2 LEDS mediante 4 botones");
 MODULE_VERSION("0.1");
 
 //Variables botones y LEDS
-static unsigned int ledRojo = 16;
-static unsigned int ledVerde = 20;
+static unsigned int ledAzul = 20;
+static unsigned int ledVerde = 16;
 static unsigned int boton1 = 21;
 static unsigned int boton2 = 26;
 static unsigned int boton3 = 19;
@@ -24,8 +24,13 @@ static bool     ledOn = 0; //Esta apagado por defecto
 
 static int __init ebbgpio_init(void){
    printk(KERN_INFO "parte1.c: Initializing parte1.c LKM\n");
-   //Comprobamos si el pin del led rojo es valido
-   if (!gpio_is_valid(ledRojo)){
+   //Comprobamos si el pin del led azul es valido
+   if (!gpio_is_valid(ledAzul)){
+      printk(KERN_INFO "parte1.c: invalid LED GPIO\n");
+      return -ENODEV;
+   }	
+   //Comprobamos si el pin del led verde es valido
+   if (!gpio_is_valid(ledVerde)){
       printk(KERN_INFO "parte1.c: invalid LED GPIO\n");
       return -ENODEV;
    }	
@@ -41,8 +46,8 @@ static void __exit ebbgpio_exit(void){
    printk(KERN_INFO "parte1.c: The button state is currently: %d\n", gpio_get_value(boton4));
    printk(KERN_INFO "parte1.c: The button was pressed %d times\n", contador4);
 
-   gpio_set_value(ledRojo, 0);              // Turn the LED off, makes it clear the device was unloaded
-   gpio_unexport(ledRojo);                  // Unexport the LED GPIO
+   gpio_set_value(ledAzul, 0);              // Turn the LED off, makes it clear the device was unloaded
+   gpio_unexport(ledAzul);                  // Unexport the LED GPIO
    gpio_set_value(ledVerde, 0);              // Turn the LED off, makes it clear the device was unloaded
    gpio_unexport(ledVerde);                  // Unexport the LED GPIO
 
@@ -51,7 +56,7 @@ static void __exit ebbgpio_exit(void){
    gpio_unexport(boton1);               // Unexport the Button GPIO
    gpio_unexport(boton2);               // Unexport the Button GPIO
 
-   gpio_free(ledRojo);                      // Free the LED GPIO
+   gpio_free(ledAzul);                      // Free the LED GPIO
    gpio_free(ledVerde);                      // Free the LED GPIO
    gpio_free(boton1);                      // Free the button GPIO
    gpio_free(boton2);                      // Free the button GPIO
