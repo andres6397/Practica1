be711fc430b3bf0c6d16c98656ce6ee536cb5bb1
andres6397 <92805319+andres6397@users.noreply.github.com>
2021-11-10 11:26:58 +0100

    Se borra archivo .Module.symvers.cmd del repositorio

--- .Module.symvers.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/pi/Practica1/Module.symvers := sed 's/ko$$/o/' /home/pi/Practica1/modules.order | scripts/mod/modpost -m -a   -o /home/pi/Practica1/Module.symvers -e -i Module.symvers   -T -
